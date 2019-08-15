8b7ed01e2d8c54567196c791f348c035a79d58f7
Inta <782720519@qq.com>
2019-08-15 12:42:02 +0800

    leetcode-practice

--- src/tencent/leetcode101_150/Q144preorderTraversal.java
+++ src/tencent/leetcode101_150/Q144preorderTraversal.java
@@ -1,7 +1,6 @@
 package tencent.leetcode101_150;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author inta
@@ -45,4 +44,50 @@ public class Q144preorderTraversal {
         }
         return list;
     }
+
+    //===============迭代======================
+    private class method2{
+        Deque<TreeNode> deque = new LinkedList<>();
+        List<Integer> res = new ArrayList<>();
+        public List<Integer> preorderTraversal(TreeNode root) {
+            if(root == null){
+                return res;
+            }
+            TreeNode cur = root;
+            while(cur!=null||!deque.isEmpty()){
+                while(cur!=null){
+                    res.add(cur.val);
+                    deque.push(cur);
+                    cur = cur.left;
+                }
+                cur = deque.pop().right;
+
+            }
+            return res;
+        }
+
+    }
+    //==============迭代2=================
+    private class method3{
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        public List<Integer> preorderTraversal(TreeNode root) {
+            if(root == null){
+                return list;
+            }
+            stack.push(root);
+            while(!stack.isEmpty()){
+                TreeNode cur = stack.pop();
+                list.add(cur.val);
+                if(cur.right!=null){
+                    stack.push(cur.right);
+                }
+                if(cur.left!=null){
+                    stack.push(cur.left);
+                }
+            }
+            return list;
+        }
+    }
 }
+
