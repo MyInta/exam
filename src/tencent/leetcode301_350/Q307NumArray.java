2fce45818ceb98a33f4cf992e8ace31f8372df21
Inta <782720519@qq.com>
2019-08-27 10:40:03 +0800

    leetcode-practice

--- src/tencent/leetcode301_350/Q307NumArray.java
+++ src/tencent/leetcode301_350/Q307NumArray.java
@@ -19,17 +19,62 @@ package tencent.leetcode301_350;
  *
  */
 public class Q307NumArray {
+    /**
+     * Your NumArray object will be instantiated and called as such:
+     * NumArray obj = new NumArray(nums);
+     * obj.update(i,val);
+     * int param_2 = obj.sumRange(i,j);
+     */
+    int[] nums;
+    public Q307NumArray(int[] nums) {
+        this.nums = nums;
+    }
 
-//    public Q307NumArray(int[] nums) {
-//
-//    }
-//
-//    public void update(int i, int val) {
-//
-//    }
-//
-//    public int sumRange(int i, int j) {
-//
-//    }
+    public void update(int i, int val) {
+        nums[i] = val;
+    }
+
+    public int sumRange(int i, int j) {
+        int res = 0;
+        for(int start = i;start<=j;start++){
+            res+=nums[start];
+        }
+        return res;
+    }
+
+    private class Q307NumArray2{
+
+        int[] nums;
+        int[] mark;
+        public Q307NumArray2(int[] nums) {
+            this.nums = nums;
+            mark = new int[nums.length+1];
+            mark[0] = 0;
+            for(int i=0;i<nums.length;i++){
+                mark[i+1] = mark[i]+nums[i];
+            }
+        }
+
+        public void update(int i, int val) {
+            int change = nums[i]-val;
+            nums[i] = val;
+            for(int start = i+1;start<=nums.length;start++){
+                mark[start]-=change;
+            }
+        }
+
+        public int sumRange(int i, int j) {
+            if(j>i){
+                return mark[j+1]-mark[i];
+            }else{
+                return nums[i];
+            }
+        }
+        //["NumArray","sumRange","update","sumRange"]
+        //[[[-1]],[0,0],[0,1],[0,0]]
+        //["NumArray","sumRange","update","sumRange"]
+        //[[[1,3,5,6,7]],[0,2],[1,2],[0,2]]
+
+    }
 
 }
