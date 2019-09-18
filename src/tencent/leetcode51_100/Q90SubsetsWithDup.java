e667cd825a647e30a4a7aff358553adc35fd18b1
Inta <782720519@qq.com>
2019-09-18 23:21:37 +0800

    leetcode-practice

--- src/tencent/leetcode51_100/Q90SubsetsWithDup.java
+++ src/tencent/leetcode51_100/Q90SubsetsWithDup.java
@@ -1,6 +1,7 @@
 package tencent.leetcode51_100;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -25,11 +26,58 @@ import java.util.List;
  *
  */
 public class Q90SubsetsWithDup {
-    List<List<Integer>> res = new ArrayList<>();
+    List<List<Integer>> res;
+    List<Integer> list;
+    boolean[] visited;
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        res.add(list);
-        if (nums.length == 0) return res;
+        res = new ArrayList<>();
+        visited = new boolean[nums.length];
+        //先将其排序，可以比较前后两个数是否一样而且是否不该取值
+        Arrays.sort(nums);
+        robot(0, nums);
         return res;
+    }
+    private void robot(int st, int[] nums) {
+        if (st > nums.length-1) {
+            list = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (visited[i]) {
+                    list.add(nums[i]);
+                }
+            }
+            res.add(list);
+            return;
+        }
+        if (st > 0&& nums[st - 1] == nums[st] && visited[st - 1] == false) {
+            visited[st] = false;
+            robot(st + 1, nums);
+        } else {
+            visited[st] = true;
+            robot(st + 1, nums);
+            visited[st] = false;
+            robot(st + 1, nums);
+        }
+    }
+
+    private class Q90SubsetsWithDup2 {
+        private List<List<Integer>> res;
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            res = new ArrayList<>();
+            Arrays.sort(nums);
+            solution(0, new ArrayList<>(), nums);
+            return res;
+        }
+        private void solution(int st, ArrayList<Integer> list, int[] nums) {
+            res.add(new ArrayList<>(list));
+            for (int i = st; i < nums.length; i++) {
+                if (i > st && nums[i -1] == nums[i]) {
+                    continue;
+                }
+                list.add(nums[i]);
+                solution(i + 1, list, nums);
+                list.remove(list.size()-1);
+            }
+        }
     }
 }
