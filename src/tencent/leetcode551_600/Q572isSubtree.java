9849ea043e6d2dc1a501278b799384056c20b138
Inta <782720519@qq.com>
2019-11-13 12:51:55 +0800

    leetcode-practice

---
+++ src/tencent/leetcode551_600/Q572isSubtree.java
@@ -0,0 +1,73 @@
+package tencent.leetcode551_600;
+
+import java.util.Stack;
+
+/**
+ * @author inta
+ * @date 2019/11/13
+ * @describe 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。
+ * s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+ *
+ * 示例 1:
+ * 给定的树 s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ * 给定的树 t：
+ *
+ *    4
+ *   / \
+ *  1   2
+ * 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+ *
+ * 示例 2:
+ * 给定的树 s：
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ *     /
+ *    0
+ * 给定的树 t：
+ *
+ *    4
+ *   / \
+ *  1   2
+ * 返回 false。
+ *
+ */
+public class Q572isSubtree {
+    private class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        return isEqual(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    /**
+     * 用来判断当值相等的情况下，子节点是否是相等的
+     * @param node1
+     * @param node2
+     * @return
+     */
+    private boolean isEqual(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) return true;
+        if (node1 == null || node2 == null) return false;
+        if (node1.val == node2.val) return isEqual(node1.left, node2.left) && isEqual(node1.right, node2.right);
+        return false;
+    }
+
+}
