01a2bfea1fe5557e5736e2e08901340d06d48f44
Inta <782720519@qq.com>
2019-11-30 10:21:26 +0800

    leetcode-practice

--- src/tencent/Main.java
+++ src/tencent/Main.java
@@ -1,11 +1,21 @@
 package tencent;
 
 
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 1;
-        System.out.println(i & 1);
+        Stack<String> test = new Stack<>();
+        test.add("a");
+        test.add("b");
+        test.add("c");
+        test.add("d");
+        for (String s : test) {
+            System.out.println(s);
+        }
+        while (!test.isEmpty()) {
+            System.out.println(test.pop());
+        }
     }
 
 }
