4488b773ff7b2d65e1bce5bd0ce160ae31b582e6
Inta <38420337+MyInta@users.noreply.github.com>
2020-04-04 18:06:21 +0800

    leetcode-practice

--- src/Main.java
+++ src/Main.java
@@ -30,6 +30,53 @@ public class Main {
 
 
 
+    /**
+     * 校验原字符串 s 是否匹配 规则 p
+     * @param s string字符串 原字符串
+     * @param p string字符串 匹配规则
+     * @return bool布尔型
+     */
+    public boolean Match (String s, String p) {
+        if (p == null || s == null) return false;
+        int index = 0, p_index = 0;
+        while (index < s.length()) {
+            //如果字符匹配，就遍历下一个
+            if (s.charAt(index) == p.charAt(p_index) || p.charAt(p_index) == '.') {
+                index ++;
+                p_index ++;
+            } else if (p.charAt(p_index) == '(') {
+                int end = p_index + 1;
+                while (p.charAt(end) != ')') {
+                    end ++;
+                }
+                //截取出字符集
+                String cutStr = p.substring(p_index + 1, end);
+                //如果是一个字符集加*的组合
+                if (end < p.length() - 1 && p.charAt(end + 1) == '*') {
+                    while (index < s.length() && isValid(s.substring(index, index + cutStr.length()), cutStr)) {
+                        //如果和p的字符集匹配，索引就往后移动
+                        index += cutStr.length();
+                    }
+                    //此时的s匹配完了，我们把p往后移动到*之后
+                    p_index += cutStr.length() + 3;
+                } else {
+                    if (!isValid(s.substring(index, index + cutStr.length()) ,cutStr)) return false;
+                    index += cutStr.length();
+                    p_index += cutStr.length() + 2;
+                }
+            } else {
+                return false;
+            }
+        }
+        return p_index == p.length() && index == s.length();
+    }
+    private boolean isValid(String str, String match) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != match.charAt(i) && match.charAt(i) != '.') return false;
+        }
+        return true;
+    }
+
 
 
     public static void main(String[] args) {
--- src/leetcode_inta/leetcode1_50/Q42trap.java
+++ src/leetcode_inta/leetcode1_50/Q42trap.java
@@ -4,7 +4,7 @@ package leetcode_inta.leetcode1_50;
  * @author inta
  * @date 2019/10/5
  * @describe 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
- * 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 感谢 Marcos 贡献此图。
+ * 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
  *
  * 示例:
  *
@@ -14,40 +14,38 @@ package leetcode_inta.leetcode1_50;
  */
 public class Q42trap {
     public int trap(int[] height) {
-        int res = 0;
-        int range = height.length;
-        if (range <= 2) return res;
-        int left = 0;
-        int right = 1;
-        int l_r_secondHigh = right;
-        while (right < range) {
-            //先记录下第二峰值
-            l_r_secondHigh = right;
-            if (height[left] > height[right]) {
-                while (right < range) {
-                    //注意，需要考虑右边遇到相等高度的情况
+        //这里我们要确认三个变量，left、right以及当前右值高峰second_right(都记录的是索引)
+        if (height == null || height.length < 3) return 0;
+        int size = height.length;
+        int left = 0, right = 1, second_right, res = 0;
+        while (right < size) {
+            //一开始第二高峰默认为right位置
+            second_right = right;
+            //如果出现凹下去的现象，就深入遍历，看能不能形成盆地
+            if (height[right] < height[left]) {
+                while (right < size) {
                     if (height[right] >= height[left]) {
-                        l_r_secondHigh = right;
+                        second_right = right;
                         break;
                     }
-                    if (height[right] >= height[l_r_secondHigh]) {
-                        //记录右边遍历过去的最大值
-                        l_r_secondHigh = right;
+                    if (height[right] >= height[second_right]) {
+                        second_right = right;
                     }
                     right ++;
                 }
+                //此时统计出一个新的盆地，或者到了尽头，不管哪种，都先累加下盆地内水量
+                res += count(height, left, second_right);
             }
-            res += count(height, left, l_r_secondHigh);
-            left = l_r_secondHigh;
+            //遍历下一个位置
+            left = second_right;
             right = left + 1;
         }
         return res;
     }
-    //默认left为短板
     private int count(int[] height, int left, int right) {
         int sum = 0;
         int minHeight = Math.min(height[left], height[right]);
-        for (int i = left + 1; i < right; i ++) {
+        for (int i = left + 1; i < right; i++) {
             sum += minHeight - height[i];
         }
         return sum;
@@ -67,7 +65,7 @@ public class Q42trap {
             if (range <= 2) return res;
             int left = 0;
             int right = 1;
-            int temp = right;
+            int temp;
             while (right < range) {
                 if (height[left] > height[right]) {
                     temp = right;
