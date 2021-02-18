9884c7d7a2f2b0399ffa20f91a6c7e9033b43e47
Inta <38420337+MyInta@users.noreply.github.com>
2021-02-18 08:31:22 +0800

    leetcode-practice 210218

---
+++ src/leetcode_inta/leetcode951_1000/Q995minKBitFlips.java
@@ -0,0 +1,53 @@
+package leetcode_inta.leetcode951_1000;
+
+/**
+ * @author inta
+ * @date 2021/2/18
+ * @describe 在仅包含 0 和 1 的数组 A 中，一次 K 位翻转包括选择一个长度为 K 的（连续）子数组，
+ * 同时将子数组中的每个 0 更改为 1，而每个 1 更改为 0。
+ * 返回所需的 K 位翻转的最小次数，以便数组没有值为 0 的元素。如果不可能，返回 -1。
+ * 示例 1：
+ *
+ * 输入：A = [0,1,0], K = 1
+ * 输出：2
+ * 解释：先翻转 A[0]，然后翻转 A[2]。
+ * 示例 2：
+ * 输入：A = [1,1,0], K = 2
+ * 输出：-1
+ * 解释：无论我们怎样翻转大小为 2 的子数组，我们都不能使数组变为 [1,1,1]。
+ * 示例 3：
+ * 输入：A = [0,0,0,1,0,1,1,0], K = 3
+ * 输出：3
+ * 解释：
+ * 翻转 A[0],A[1],A[2]: A变成 [1,1,1,1,0,1,1,0]
+ * 翻转 A[4],A[5],A[6]: A变成 [1,1,1,1,1,0,0,0]
+ * 翻转 A[5],A[6],A[7]: A变成 [1,1,1,1,1,1,1,1]
+ * 提示：
+ * 1 <= A.length <= 30000
+ * 1 <= K <= A.length
+ */
+public class Q995minKBitFlips {
+    // 需要一个数组fan[]来标记当前位置的反转奇偶性，以及一个标识符flag来判断当前位置的反转状态
+    public int minKBitFlips(int[] A, int K) {
+        int len = A.length;
+        int[] fan = new int[len];
+        int flag = 0;
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            // [TODO] 待补充，当前元素会有四种状态
+            flag ^= fan[i];
+            if (flag == A[i]) {
+                res++;
+                if (i + K > len) {
+                    return -1;
+                }
+                // flag反转
+                flag ^= 1;
+                if (i + K < len) {
+                    fan[i + K] ^= 1;
+                }
+            }
+        }
+        return res;
+    }
+}
