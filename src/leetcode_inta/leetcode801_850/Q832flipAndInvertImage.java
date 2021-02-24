811d66f7abfbb2de0d12f329730801959e2c10a2
Inta <38420337+MyInta@users.noreply.github.com>
2021-02-24 08:14:59 +0800

    leetcode-practice 210224

--- src/leetcode_inta/leetcode801_850/Q832flipAndInvertImage.java
+++ src/leetcode_inta/leetcode801_850/Q832flipAndInvertImage.java
@@ -55,4 +55,37 @@ public class Q832flipAndInvertImage {
         }
         return A;
     }
+
+    public int[][] flipAndInvertImage2(int[][] A) {
+        int m = A.length;
+        int n = A[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = A[i][j] ^ 1;
+                A[i][j] = A[i][n - 1 - j] ^ 1;
+                A[i][n - 1 - j] = temp;
+            }
+            if ((n & 1) == 1) {
+                A[i][n / 2] ^= 1;
+            }
+        }
+        return A;
+    }
+
+    public int[][] flipAndInvertImage3(int[][] A) {
+        int m = A.length;
+        int n = A[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                if (A[i][j] == A[i][n - 1 - j]) {
+                    A[i][j] ^= 1;
+                    A[i][n - 1 - j] ^= 1;
+                }
+            }
+            if ((n & 1) == 1) {
+                A[i][n / 2] ^= 1;
+            }
+        }
+        return A;
+    }
 }
