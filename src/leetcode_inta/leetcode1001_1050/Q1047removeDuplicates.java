929596e4171b495b49194d9de3d2d1518e4df85a
Inta <38420337+MyInta@users.noreply.github.com>
2021-03-09 08:09:43 +0800

    leetcode-practice 210309

--- src/leetcode_inta/leetcode1001_1050/Q1047removeDuplicates.java
+++ src/leetcode_inta/leetcode1001_1050/Q1047removeDuplicates.java
@@ -6,30 +6,20 @@ import java.util.Stack;
  * @author inta
  * @date 2019/12/25
  * @describe 给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
- *
  * 在 S 上反复执行重复项删除操作，直到无法继续删除。
- *
  * 在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
- *
- *  
- *
  * 示例：
- *
  * 输入："abbaca"
  * 输出："ca"
  * 解释：
  * 例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，
  * 这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
- *  
- *
  * 提示：
- *
  * 1 <= S.length <= 20000
  * S 仅由小写英文字母组成。
- *
  */
 public class Q1047removeDuplicates {
-    //set，从左往右找过去一遍
+    // set，从左往右找过去一遍
     public String removeDuplicates(String S) {
         Stack<Character> stack = new Stack<>();
         char[] s_chars = S.toCharArray();
@@ -47,22 +37,35 @@ public class Q1047removeDuplicates {
         return sb.toString();
     }
 
-    //如果我不使用stack可以实现吗？指针？
+    // 如果我不使用stack可以实现吗？指针？
     public String removeDuplicates2(String S) {
-        //使用指针来保留回退
+        // 使用指针来保留回退
         int left = 0;
-        //装载，类似于stack的替换
+        // 装载，类似于stack的替换
         char[] temp = new char[S.length()];
-        for (int i = 0; i < S.length(); i ++) {
+        for (int i = 0; i < S.length(); i++) {
             //当遍历到和前一个元素相同情况下，我们回退指针
             if (left != 0 && S.charAt(i) == temp[left - 1]) {
-                left --;
+                left--;
             } else {
-                //否则，就保留这个元素
-                temp[left ++] = S.charAt(i);
+                // 否则，就保留这个元素
+                temp[left++] = S.charAt(i);
             }
         }
-        //此时，指针是在实际我们需要保存的数组索引后一位
+        // 此时，指针是在实际我们需要保存的数组索引后一位
         return String.valueOf(temp).substring(0, left);
+    }
+
+    public String removeDuplicates3(String S) {
+        int cur = 0;
+        char[] chars = new char[S.length()];
+        for (char c : S.toCharArray()) {
+            if (cur != 0 && chars[cur - 1] == c) {
+                cur--;
+            } else {
+                chars[cur++] = c;
+            }
+        }
+        return String.valueOf(chars).substring(0, cur);
     }
 }
