9dc4e1a5f106c15b6deb81cd3acf5351d3a9e6a8
Inta <38420337+MyInta@users.noreply.github.com>
2021-03-11 08:37:16 +0800

    leetcode-practice 210311

--- src/leetcode_inta/leetcode201_250/Q227calculate.java
+++ src/leetcode_inta/leetcode201_250/Q227calculate.java
@@ -7,39 +7,33 @@ import java.util.Stack;
  * @author inta
  * @date 2019/10/29
  * @describe 实现一个基本的计算器来计算一个简单的字符串表达式的值。
- *
  * 字符串表达式仅包含非负整数，+， - ，*，/ 四种运算符和空格  。 整数除法仅保留整数部分。
- *
  * 示例 1:
- *
  * 输入: "3+2*2"
  * 输出: 7
  * 示例 2:
- *
  * 输入: " 3/2 "
  * 输出: 1
  * 示例 3:
- *
  * 输入: " 3+5 / 2 "
  * 输出: 5
  * 说明：
- *
  * 你可以假设所给定的表达式都是有效的。
  * 请不要使用内置的库函数 eval。
  */
 public class Q227calculate {
     public int calculate(String s) {
         String clean_s = s.replace(" ","");
-        //队列，方便后续从头开始遍历
+        // 队列，方便后续从头开始遍历
         LinkedList<String> queue = new LinkedList<>();
-        for (int i = 0; i < clean_s.length(); i ++) {
+        for (int i = 0; i < clean_s.length(); i++) {
             char temp = clean_s.charAt(i);
             if (temp == '+' || temp == '-') {
-                //说明是运算符
+                // 说明是运算符
                 queue.add(String.valueOf(temp));
                 System.out.println(String.valueOf(temp));
             } else if (temp == '*' || temp == '/') {
-                //乘除优先计算
+                // 乘除优先计算
                 String last = queue.pollLast();
                 int isNum = 0;
                 i ++;
@@ -86,12 +80,12 @@ public class Q227calculate {
         return res;
     }
 
-    //网上解答比自己写的思路清晰，且效率高，核心在于用stack保存值，而遍历每个运算符号
+    // 网上解答比自己写的思路清晰，且效率高，核心在于用stack保存值，而遍历每个运算符号
     public int calculate2(String s) {
         Stack<Integer> stack = new Stack<>();
         int num = 0;
         char sigh = '+';
-        for (int i =0; i < s.length(); i ++) {
+        for (int i =0; i < s.length(); i++) {
             char temp = s.charAt(i);
             if (temp >= '0') {
                 num = num * 10 + (temp - '0');
@@ -112,5 +106,55 @@ public class Q227calculate {
             res += i;
         }
         return res;
+    }
+
+    public int calculate3(String s) {
+        Stack<Integer> res = new Stack<>();
+        int curNum = 0;
+        char calMark = '+';
+        for (char c : s.toCharArray()) {
+            if (c >= '0') {
+                curNum = curNum * 10 + (c - '0');
+            }
+
+            if (c < '0' && c != ' ') {
+                switch(calMark) {
+                    case '+':
+                        res.push(curNum);
+                        break;
+                    case '-':
+                        res.push(-curNum);
+                        break;
+                    case '*':
+                        res.push(res.pop() * curNum);
+                        break;
+                    case '/':
+                        res.push(res.pop() / curNum);
+                        break;
+                }
+                calMark = c;
+                curNum = 0;
+            }
+
+        }
+        switch(calMark) {
+            case '+':
+                res.push(curNum);
+                break;
+            case '-':
+                res.push(-curNum);
+                break;
+            case '*':
+                res.push(res.pop() * curNum);
+                break;
+            case '/':
+                res.push(res.pop() / curNum);
+                break;
+        }
+        int reslut = 0;
+        for (int num : res) {
+            reslut += num;
+        }
+        return reslut;
     }
 }
