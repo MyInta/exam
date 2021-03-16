dad3e211f99c9c762022d032e0f291fd12f55383
Inta <38420337+MyInta@users.noreply.github.com>
2021-03-16 08:00:06 +0800

    leetcode-practice 210316

--- src/leetcode_inta/leetcode51_100/Q59generateMatrix.java
+++ src/leetcode_inta/leetcode51_100/Q59generateMatrix.java
@@ -5,9 +5,7 @@ package leetcode_inta.leetcode51_100;
  * @author inta
  * @date 2019/9/28
  * @describe 给定一个正整数 n，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
- *
  * 示例:
- *
  * 输入: 3
  * 输出:
  * [
@@ -15,7 +13,6 @@ package leetcode_inta.leetcode51_100;
  *  [ 8, 9, 4 ],
  *  [ 7, 6, 5 ]
  * ]
- *
  */
 public class Q59generateMatrix {
     public int[][] generateMatrix(int n) {
@@ -43,6 +40,37 @@ public class Q59generateMatrix {
         }
         if (start == end) {
             res[start-1][start-1] = n*n;
+        }
+        return res;
+    }
+
+    public int[][] generateMatrix2(int n) {
+        int cur = 1;
+        int[][] res = new int[n][n];
+        int left = 0;
+        int right = n - 1;
+        int top = 0;
+        int bottom = n - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                res[top][i] = cur++;
+            }
+            for (int i = top + 1; i <= bottom; i++) {
+                res[i][right] = cur++;
+            }
+            if (left > right || top > bottom) {
+                break;
+            }
+            right--;
+            top++;
+            for (int i = right; i >= left; i--) {
+                res[bottom][i] = cur++;
+            }
+            for (int i = bottom - 1; i >= top; i--) {
+                res[i][left] = cur++;
+            }
+            bottom--;
+            left++;
         }
         return res;
     }
