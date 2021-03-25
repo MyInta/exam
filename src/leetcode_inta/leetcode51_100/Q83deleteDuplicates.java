44543c62b309325d26014e2ecaf88413831fc4eb
Inta <38420337+MyInta@users.noreply.github.com>
2021-03-26 07:54:00 +0800

    leetcode-practice 210326

--- src/leetcode_inta/leetcode51_100/Q83deleteDuplicates.java
+++ src/leetcode_inta/leetcode51_100/Q83deleteDuplicates.java
@@ -4,16 +4,12 @@ package leetcode_inta.leetcode51_100;
  * @author inta
  * @date 2019/11/18
  * @describe 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
- *
  * 示例 1:
- *
  * 输入: 1->1->2
  * 输出: 1->2
  * 示例 2:
- *
  * 输入: 1->1->2->3->3
  * 输出: 1->2->3
- *
  */
 public class Q83deleteDuplicates {
     private class ListNode{
@@ -36,5 +32,21 @@ public class Q83deleteDuplicates {
             }
         }
         return head;
+    }
+
+    public ListNode deleteDuplicates2(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = head;
+        ListNode next = head;
+        while (next != null) {
+            next = next.next;
+            while (next != null && next.val == cur.val) {
+                next = next.next;
+            }
+            cur.next = next;
+            cur = next;
+        }
+        return dummy.next;
     }
 }
