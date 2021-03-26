4454105f598711aaad55b6496d22f0c61f759801
Inta <38420337+MyInta@users.noreply.github.com>
2021-03-26 08:18:33 +0800

    leetcode-practice 210326

--- src/leetcode_inta/leetcode51_100/Q83deleteDuplicates.java
+++ src/leetcode_inta/leetcode51_100/Q83deleteDuplicates.java
@@ -20,6 +20,7 @@ public class Q83deleteDuplicates {
         }
     }
 
+    // 非递归体，考虑两两链表节点间的去重判断
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) return head;
         ListNode cur = head;
@@ -34,6 +35,7 @@ public class Q83deleteDuplicates {
         return head;
     }
 
+    // 非递归体，添加while判断，对同一值的链表节点一次去重
     public ListNode deleteDuplicates2(ListNode head) {
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
@@ -50,6 +52,7 @@ public class Q83deleteDuplicates {
         return dummy.next;
     }
 
+    // 正向递归，考虑进入递归的下个链表节点和当前链表节点关系
     public ListNode deleteDuplicates3(ListNode head) {
         if (head == null || head.next == null) {
             return head;
@@ -59,6 +62,18 @@ public class Q83deleteDuplicates {
             deleteDuplicates(head);
         } else {
             deleteDuplicates(head.next);
+        }
+        return head;
+    }
+
+    // 反向递归，一路递归到底后，层层回归，并考虑返回去重后的链表头
+    public ListNode deleteDuplicates4(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        head.next = deleteDuplicates(head.next);
+        if (head.val == head.next.val) {
+            head = head.next;
         }
         return head;
     }
