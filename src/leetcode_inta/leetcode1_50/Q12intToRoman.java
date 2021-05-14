39885cf1a8ac3c7d2a128829756f6442e60e1bbe
Inta <38420337+MyInta@users.noreply.github.com>
2021-05-14 08:32:33 +0800

    leetcode-practice 210514

--- src/leetcode_inta/leetcode1_50/Q12intToRoman.java
+++ src/leetcode_inta/leetcode1_50/Q12intToRoman.java
@@ -142,4 +142,74 @@ public class Q12intToRoman {
         }
         return sb.toString();
     }
+
+    public String intToRoman2(int num) {
+        StringBuilder sb = new StringBuilder();
+        while (num >= 1000) {
+            sb.append('M');
+            num -= 1000;
+        }
+        if (num >= 900) {
+            sb.append("CM");
+            num %= 900;
+        }
+        if (num >= 500) {
+            sb.append("D");
+            num -= 500;
+        }
+        if (num >= 400) {
+            sb.append("CD");
+            num %= 400;
+        }
+        while (num >= 100) {
+            sb.append("C");
+            num -= 100;
+        }
+        if (num >= 90) {
+            sb.append("XC");
+            num %= 90;
+        }
+        if (num >= 50) {
+            sb.append("L");
+            num -= 50;
+        }
+        if (num >= 40) {
+            sb.append("XL");
+            num %= 40;
+        }
+        while (num >= 10) {
+            sb.append("X");
+            num -= 10;
+        }
+        if (num >= 9) {
+            sb.append("IX");
+            num %= 9;
+        }
+        if (num >= 5) {
+            sb.append("V");
+            num -= 5;
+        }
+        if (num >= 4) {
+            sb.append("IV");
+            num %= 4;
+        }
+        while (num >= 1) {
+            sb.append("I");
+            num--;
+        }
+        return sb.toString();
+    }
+
+    public String intToRoman3(int num) {
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] strs = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            while (num >= nums[i]) {
+                sb.append(strs[i]);
+                num -= nums[i];
+            }
+        }
+        return sb.toString();
+    }
 }
