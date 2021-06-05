fc55fc549c2959b20f6e9f0d430c4be7b1fdc5bf
Inta <38420337+MyInta@users.noreply.github.com>
2021-06-05 08:13:57 +0800

    leetcode-practice 210605

--- src/leetcode_inta/leetcode201_250/Q203removeElements.java
+++ src/leetcode_inta/leetcode201_250/Q203removeElements.java
@@ -16,33 +16,25 @@ public class Q203removeElements {
     }
 
     public ListNode removeElements(ListNode head, int val) {
-        if (head == null) return null;
-        while (head != null && head.val == val) head = head.next;
-        ListNode temp = head;
-        while (temp != null && temp.next != null) {
-            if (temp.next.val == val) {
-                temp.next = temp.next.next;
-            } else {
-                temp = temp.next;
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        dummy.next = head;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+                continue;
             }
+            cur = cur.next;
         }
-        return head;
+        return dummy.next;
     }
 
-    //递归
+    // 递归
     public ListNode removeElements2(ListNode head, int val) {
-        if (head == null) return null;
-//        if (head.val == val) return removeElements(head.next, val);
-//        ListNode pre = head;
-//        while (head.next != null) {
-//            if (head.next.val == val) {
-//                head.next = head.next.next;
-//            } else {
-//                head = head.next;
-//            }
-//        }
-//        return pre;
-        //倒着找，从末端null往上遍历
+        if (head == null) {
+            return null;
+        }
+        // 倒着找，从末端null往上遍历
         head.next = removeElements2(head.next, val);
         if (head.val == val) {
             return head.next;
